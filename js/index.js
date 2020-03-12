aa765079c8e35c9f821ee4166f4cf7138bee2ab1
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-03-12 00:38:06 -0400

    index.js

--- js/index.js
+++ js/index.js
@@ -38,5 +38,38 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// ## Task 1: Create selectors to point your data into elements
+// * [ ] Create selectors by using any of the DOM element's methods
+// * [ ] Note that IDs have been used on all images.  Use the IDs to update src path content
+
+const headerline = document.getElementById("cta-img");
+headerline.setAttribute('src', siteContent["cta"]["img-src"])
+
+// ## Task 2: Update the HTML with the JSON data
+// * [ ] Remember, NO direct updating of the HTML source is allowed.
+// * [ ] Using your selectors, update the content to match the example file.
+// * [ ] Remember to update the src attributes on images
+
+const navbar = document.getElementsByTagName("a");
+//navbar.setAttribute('src', siteContent["nav"]["nav-item-1"]);
+
+
+const middleImge = document.getElementById("middle-img");
+middleImge.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+// ## Task 3: Add new content
+// * [ ] Change the color of the navigation text to be green.
+// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+// * [ ] Check your work by looking at the [original html](original.html) in the browser
+
+// ## Stretch Goals
+// * [ ] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM using style in JavaScript.  
+// * [ ] Study tomorrow's lesson on events and try to integrate a button that can update content on the site with a click of a button.  You could build a similar data object with new values to help you test the click event.
+
+// ## Stretch Project: Digital Timer
+// This project is heavier on logic but employs some DOM manipulation to achieve it's goals.  Go check it out here: [stretch assignment](stretch-assignment) and see how far you can get. 
